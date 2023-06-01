b88fd4eaf0487b1d41ee1e208853656d787310af
Nick Welch <nwelchr@gmail.com>
2023-06-01 16:30:21 -0700

    Generate Card

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,30 @@ import React from "react";
 import "./App.css";
 import images from "./assets/index";
 
+const imageUrls = Object.values(images);
+
+function CardGenerator() {
+  const [card, setCard] = React.useState("");
+
+  const getRandomCard = () => {
+    const randomIndex = Math.floor(Math.random() * imageUrls.length);
+    const randomCard = imageUrls[randomIndex];
+    setCard(randomCard);
+  };
+
+  return (
+    <section>
+      <img src={card} />
+      <button onClick={getRandomCard}>Generate Card</button>
+    </section>
+  );
+}
+
 function App() {
   return (
     <main className="app">
-      {Object.values(images).map((image, index) => {
+      <CardGenerator />
+      {imageUrls.map((image, index) => {
         return <img src={image} key={index} />;
       })}
     </main>
